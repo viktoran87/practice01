17a38b04f34016f4e7b829a1ee31ef53a735709c
viktoran87 <Vitek260387@rambler.ru>
2024-08-22 00:39:07 +0300

    Add files via upload

---
+++ practice02.py
@@ -0,0 +1,13 @@
+# "1st program"
+print((9**0.5) * 5)
+# "2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+# "4th program"
+example = ('123,456')
+print(example[4])
+# "4th program" 2nd option
+print(int((123.456*10)%10))
